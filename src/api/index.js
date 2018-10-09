fe875b21cb699f1279ad15f251dc8edd0c45e064
wxmid <wuxmid@163.com>
2018-10-09 23:40:05 +0800

    update

--- src/api/index.js
+++ src/api/index.js
@@ -1,112 +1,50 @@
-/**
- * Created by wxm-pc on 2018/10/8.
- */
-// 引入axios
 import axios from 'axios'
-let cancel, promiseArr = {}
-const CancelToken = axios.CancelToken
-// 请求拦截器
-axios.interceptors.request.use(config => {
-  // 发起请求时，取消掉当前正在进行的相同请求
-  if (promiseArr[config.url]) {
-    promiseArr[config.url]('操作取消')
-    promiseArr[config.url] = cancel
-  } else {
-    promiseArr[config.url] = cancel
-  }
-  return config
-}, error => {
-  return Promise.reject(error)
-})
-
-// 响应拦截器即异常处理
-axios.interceptors.response.use(response => {
-  return response
-}, err => {
-  if (err && err.response) {
-    switch (err.response.status) {
-      case 400:
-        err.message = '错误请求'
-        break;
-      case 401:
-        err.message = '未授权，请重新登录'
-        break;
-      case 403:
-        err.message = '拒绝访问'
-        break;
-      case 404:
-        err.message = '请求错误,未找到该资源'
-        break;
-      case 405:
-        err.message = '请求方法未允许'
-        break;
-      case 408:
-        err.message = '请求超时'
-        break;
-      case 500:
-        err.message = '服务器端出错'
-        break;
-      case 501:
-        err.message = '网络未实现'
-        break;
-      case 502:
-        err.message = '网络错误'
-        break;
-      case 503:
-        err.message = '服务不可用'
-        break;
-      case 504:
-        err.message = '网络超时'
-        break;
-      case 505:
-        err.message = 'http版本不支持该请求'
-        break;
-      default:
-        err.message = `连接错误${err.response.status}`
-    }
-  } else {
-    err.message = "连接到服务器失败"
-  }
-  message.err(err.message)
-  return Promise.resolve(err.response)
-})
-
-axios.defaults.baseURL = '/api'
-//设置默认请求头
-axios.defaults.headers = {
-  'X-Requested-With': 'XMLHttpRequest'
+import qs from 'qs'
+axios.defaults.timeout = 15000
+let pathname = ''
+let path = location.pathname
+if (path) {
+  console.log(path)
+  pathname = path.split('/')[1]
+  console.log(pathname)
 }
-axios.defaults.timeout = 10000
-
-export default {
-  //get请求
-  get (url,param) {
-    return new Promise((resolve,reject) => {
-      axios({
-        method: 'get',
-        url,
-        params: param,
-        cancelToken: new CancelToken(c => {
-          cancel = c
-        })
-      }).then(res => {
-        resolve(res)
-      })
-    })
-  },
-  // post请求
-  post (url,param) {
-    return new Promise((resolve,reject) => {
-      axios({
-        method: 'post',
-        url,
-        data: param,
-        cancelToken: new CancelToken(c => {
-          cancel = c
-        })
-      }).then(res => {
-        resolve(res)
-      })
-    })
+axios.defaults.baseURL = '/' + pathname
+axios.defaults.headers = {
+  'Accept': 'application/json;charset=utf-8',
+  'Content-Type': 'application/json;charset=utf-8'
+}
+const formdateConfig = {
+  headers: {
+    'Content-Type': 'multipart/formDate'
   }
+}
+axios.interceptors.request.use(
+  config => {
+    if (config.method === 'post') {
+      config.data = JSON.stringify(config.data)
+    } else if (config.method === 'get') {
+      config.data = qs.stringify(config.data)
+    }
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+axios.interceptors.response.use(
+  response => {
+    return response
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+function post (path, params) {
+  return axios.post(path, params)
+}
+function upload (path, params) {
+  return axios.post(path, params, formdateConfig)
+}
+function get (path, params) {
+  return axios.get(path, params)
 }
