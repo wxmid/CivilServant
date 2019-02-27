6e0440cecb02ec4c1902525be02fc8edf6b903d8
wxmid <wuxmid@163.com>
2019-02-27 23:57:17 +0800

    update config

---
+++ src/config/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by wxm-pc on 2019/1/5.
+ */
+export default  {
+  prompt: {
+    errorTxt: '网络开小差啦，请稍后再试...'
+  },
+  // 本地环境
+  /*glolal: {
+    baseUrl: 'http://localhost:3000'
+  },*/
+  // 正式环境
+  glolal: {
+    baseUrl: 'http://www.mindwen.com:3000'
+  },
+}
